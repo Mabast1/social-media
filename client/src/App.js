e301756714deb090c600361dcdc206335725d114
Mabast <mabast.niga@yahoo.com>
2022-06-15 21:49:11 -0500

    Created Home component and added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container } from "@material-ui/core";
 
-import { getPosts } from "./actions/posts";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
 import Navbar from "./components/Navbar/Navbar";
-import useStlyes from "./styles";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStlyes();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   return (
-    <Container maxWidth="lg">
-      <Navbar />
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            className={classes.mainContainer}
-            justify="space-between"
-            alignItems=" stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Routes>
+        <Home />
+      </Container>
+    </BrowserRouter>
   );
 };
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 
-import useStlyes from "./styles";
 import memories from "../../images/memories.png";
+import useStyles from "./styles";
 
 const Navbar = () => {
-  const classes = useStlyes;
+  const classes = useStyles();
+
+  const user = null;
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
@@ -27,7 +29,41 @@ const Navbar = () => {
           height="60"
         />
       </div>
-      <Toolbar className={classes.toolbar}></Toolbar>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <>
+            <div className={classes.profile}>
+              <Avatar
+                className={classes.purple}
+                alt={user.result.name}
+                src={user.result.imageUrl}
+              >
+                {user.result.name.charAt(0)}
+              </Avatar>
+            </div>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+              onClick={() => {}}
+            >
+              Logout
+            </Button>
+          </>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
     </AppBar>
   );
 };
