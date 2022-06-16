139e4365358416ff56fae63a8aaae2ece7fbd6d6
Mabast <mabast.niga@yahoo.com>
2022-06-15 20:35:52 -0500

    updated dotenv

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,8 +16,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 //mongo db cloud atlas setup
-const CONNECTION_URL =
-  "mongodb+srv://mabast:HM1995mn1@cluster0.46cblav.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
