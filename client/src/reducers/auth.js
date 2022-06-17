4daf9e98b7724a2c504aa5e60fe97953d4829123
Mabast <mabast.niga@yahoo.com>
2022-06-16 23:52:09 -0500

    code cleanup

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,9 +4,7 @@ const authReducer = (state = { authData: null }, action) => {
   switch (action.type) {
     case AUTH:
       console.log(action?.data);
-
       return state;
-
     default:
       return state;
   }
