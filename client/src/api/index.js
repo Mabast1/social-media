85d414793f2b1813c3ce7b6e3f23710832b25d4f
Mabast <mabast.niga@yahoo.com>
2022-06-20 22:56:14 -0500

    created sign in and sign up dispatches on the frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -8,3 +8,6 @@ export const updatePost = (id, updatedPost) =>
   API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+
+export const signIn = (formData) => API.post("/users/signin", formData);
+export const signUp = (formData) => API.post("/users/signup", formData);
