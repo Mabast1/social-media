eaf510d8969b3c69fb12fa3acbf95bd270a3af27
Mabast <mabast.niga@yahoo.com>
2022-06-27 00:06:10 -0500

    Created routes for Post Details (new component)

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Container } from "@material-ui/core";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={() => <Navigate to="/posts" />} />
           <Route path="/posts" element={<Home />} />
           <Route path="/posts/search" element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
           <Route path="/auth" element={<Auth />} />
         </Routes>
       </Container>
