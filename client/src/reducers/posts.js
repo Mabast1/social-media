3798a00d57ede5af6a99dee76500f3ff3b43af86
Mabast <mabast.niga@yahoo.com>
2022-06-29 22:58:19 -0500

    adding loading and endloading states

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,11 +4,18 @@ import {
   CREATE,
   UPDATE,
   DELETE,
+  LIKE,
+  START_LOADING,
+  END_LOADING,
 } from "../constants/actionTypes";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (state = [], action) => {
+export default (state = { isLoading: true, posts: [] }, action) => {
   switch (action.type) {
+    case START_LOADING:
+      return { ...state, isLoading: true };
+    case END_LOADING:
+      return { ...state, isLoading: false };
     case FETCH_ALL:
       return {
         ...state,
@@ -21,11 +28,24 @@ export default (state = [], action) => {
     case CREATE:
       return [...state, action.payload];
     case UPDATE:
-      return state.map((post) =>
-        post._id === action.payload._id ? action.payload : post
-      );
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === action.payload._id ? action.payload : post
+        ),
+      };
+    case LIKE:
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === action.payload._id ? action.payload : post
+        ),
+      };
     case DELETE:
-      return state.filter((post) => post._id !== action.payload);
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== action.payload),
+      };
     default:
       return state;
   }
